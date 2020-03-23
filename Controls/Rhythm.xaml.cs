30a49b9e29c3c452bcb3b60c970f5d3e2f4badf1
Антон <Stan614@yandex.ru>
2020-03-23 16:18:28 +0300

    GTG

---
+++ Controls/Rhythm.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoordinationTraining.Controls
+{
+    public partial class Rhythm : UserControl
+    {
+        public Rhythm()
+        {
+            InitializeComponent();
+        }
+
+        private void HiHat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Snare_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RotoTom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FloorTom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
