53585b66eed66baff1d01a4d137ecd7e0c96c05e
Антон <Stan614@yandex.ru>
2020-03-23 19:12:13 +0300

    ккк

---
+++ Controls/Casses/Tact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoordinationTraining.Controls.Casses
+{
+    public class Tact
+    {
+        bool HiHat,
+             Snare,
+             MiddleTom,
+             HightTom,
+             FloorTom,
+             BassDrum;
+
+    }
+}
--- Controls/Rhythm.xaml.cs
+++ Controls/Rhythm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +18,39 @@ namespace CoordinationTraining.Controls
 {
     public partial class Rhythm : UserControl
     {
+        ObservableCollection<Grid> GridEventsColl;
+        ObservableCollection<(Grid, Grid, Grid, Grid, Grid)> GridEventsCol1l;
         public Rhythm()
         {
             InitializeComponent();
+            InitColls();
+        }
+
+        void InitColls()
+        {
+            GridEventsColl = new ObservableCollection<Grid>();
+            GridEventsColl.Add(Menu_0);
+            GridEventsColl.Add(Menu_1);
         }
 
         private void HiHat_Click(object sender, RoutedEventArgs e)
         {
-
+            MenuItem mnu = sender as MenuItem;
+            Grid sp = null;
+            if (mnu != null)
+            {
+                sp = ((ContextMenu)mnu.Parent).PlacementTarget as Grid;
+            }
+            int i = Convert.ToInt32(sp.Name[sp.Name.Length - 1] - 48);
+            
+            if (GridEventsColl[i].Visibility == Visibility.Visible)
+            {
+                GridEventsColl[i].Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                GridEventsColl[i].Visibility = Visibility.Visible;
+            }
         }
 
         private void Snare_Click(object sender, RoutedEventArgs e)
